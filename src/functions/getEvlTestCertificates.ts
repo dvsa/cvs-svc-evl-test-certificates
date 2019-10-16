9289370748dc89b73aefef3b6d066119c3deb850
Umer Jaffer <ujaffer@deloitte.co.uk>
2019-10-16 12:25:45 +0100

    complete logic with stub data for CVSB-8179

---
+++ src/functions/getEvlTestCertificates.ts
@@ -0,0 +1,13 @@
+import {EvlTestCertificatesDAO} from "../models/EvlTestCertificatesDAO";
+import {EvlTestCertificatesService} from "../services/EvlTestCertificatesService";
+import { HTTPResponse } from "../models/HTTPResponse";
+
+export const getEvlTestCertificates = () => {
+  const evlTestCertificatesDAO = new EvlTestCertificatesDAO();
+  const evlTestCertificatesService = new EvlTestCertificatesService(evlTestCertificatesDAO);
+
+  return evlTestCertificatesService.getEvlTestCertificates().then((data) => {
+      return new HTTPResponse(200, data);
+    });
+
+};
